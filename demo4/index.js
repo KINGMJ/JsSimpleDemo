51f2ab400aa6ed38f13951c985f5b02537dd7e24
JerryMei <328047478@qq.com>
2019-06-05 15:43:18 +0800

    新增demo：websocket重连

---
+++ demo4/index.js
@@ -0,0 +1,39 @@
+var ws;
+// var wsUrl = 'ws://192.168.0.100/';
+var wsUrl = 'wss://rs.leangoo.com/';
+
+function createWebSocket(url) {
+    ws = new WebSocket(url);
+    initEventHandle();
+}
+
+function initEventHandle() {
+    ws.onopen = function (ev) {
+        console.log("开始建立连接");
+        // ws.send(JSON.stringify(["5", "board1441360"]));
+        ws.send(JSON.stringify(["5", "board2805625"]));
+    };
+
+    ws.onmessage = function (ev) {
+        document.getElementById("container").innerHTML += "使用原生的：<pre>" + ev.data + "</pre>";
+    };
+
+    ws.onclose = function (ev) {
+        console.log('websocket 断开: ' + ev.code + ' ' + ev.reason + ' ' + ev.wasClean);
+        console.log(ev);
+        reconnect(wsUrl);
+    };
+
+    ws.onerror = function (ev) {
+        console.log('websocket服务出错了');
+        console.log(ev);
+        reconnect(wsUrl);
+    };
+}
+
+function reconnect(url) {
+    createWebSocket(url);
+}
+
+
+createWebSocket(wsUrl);
